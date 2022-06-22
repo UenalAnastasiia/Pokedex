3be1d53a7d87698fb07d9ca9a45b8e36d869b823
UenalAnastasiia <velyka.nastya@icloud.com>
2022-06-22 19:49:30 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let currentPokemon;
 let pokemonStart = 0;
 let loadedPokemonNumber = 26;
-let limit = 650;
+let pokemonLimit = 650;
 
-//############# Scroll-Event for Loading more Pokemons ############# 
+/**
+ * Scroll-Event for Loading more Pokemons
+ */
 window.addEventListener('scroll', () => {
     const scrollable = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
 
     if (Math.ceil(scrolled) === scrollable) {
-        if (loadedPokemonNumber < limit) {
+        if (loadedPokemonNumber < pokemonLimit) {
             loadedPokemonNumber += 25;
             pokemonStart += 25;
             renderPokedex();
@@ -18,13 +20,14 @@ window.addEventListener('scroll', () => {
 });
 
 
-//############# Fetch API => load Pokemon-Dates from API ############# 
+/**
+ * Fetch API => load Pokemon-Dates from API
+ */
 async function renderPokedex() {
     for (let i = pokemonStart + 1; i < loadedPokemonNumber; i++) {
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
-        console.log('Loaded pokemon', currentPokemon);
 
         renderPokemonInfo(i);
     }
@@ -50,7 +53,9 @@ function closeCard(i) {
 }
 
 
-//############# Switch between Info-Box "About" and Info-Box "Stats" ############# 
+/**
+ * Switch between Info-Box "About" and Info-Box "Stats"
+ */
 function chooseBoxInOpenCard() {
     if (document.getElementById('info-stats').classList.contains('d-none') == true) {
         document.getElementById('info-about').classList.add('d-none');
@@ -66,98 +71,23 @@ function chooseBoxInOpenCard() {
 }
 
 
-//############# Generate HTML ############# 
-function generatePokemonInfoHTML(i) {
-    return /*html*/`
-    <div onclick="openCard(${i})" class="pokemonCard" id="pokemonCard${i}" style="background-color: var(--bg-${currentPokemon['types'][0]['type']['name']})">
-        <div class="pokemon-header">
-            <h2>${currentPokemon['name']}</h2>
-        </div>
-        <div class="img-box">
-            <img class="pokemon-img" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}">
-        </div>
-        <div class="pokemon-type-id-box">
-            <div class="pokemon-type-id">
-                <h2>${currentPokemon['types'][0]['type']['name']}</h2>
-            </div>
-            <div class="pokemon-type-id">
-                <h2>#${currentPokemon['id']}</h2>
-            </div>
-        </div>
-    </div>
-    `;
-}
+/**
+ * Search Pokemon
+ */
+function searchPokemon() {
+    const search = document.getElementById("search").value;
 
+    for (let i = 1; i < pokemonLimit; i++) {
+        let pokemonCard = document.getElementById(`pokemonCard${i}`);
+        let pokemonName = document.getElementById(`pokemonName${i}`).innerHTML;
 
-function generateOpenCardHTML(i) {
-    return /*html*/ `
-    <div class="big-card-bg" id="big-card">
-        <div class="openCard big-card margin-0 width-400" id="pokemonCard${i}" style="background-color: var(--bg-${currentPokemon['types'][0]['type']['name']})">
-            <div class="card-top">
-                <div class="header-icons">
-                    <img src="img/save.svg">
-                    <img onclick="closeCard(${i})" src="img/x-mark.png">
-                </div>
-
-                <div class="pokemon-header">
-                    <h2 class="header-open-card">${currentPokemon['name']}</h2>
-                </div>
-                <div class="img-box width-400">
-                    <img class="pokemon-img" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}">
-                </div>
-                <div class="pokemon-type-id-box">
-                    <div class="pokemon-type-id type-margin">
-                        <h2>${currentPokemon['types'][0]['type']['name']}</h2>
-                    </div>
-                    <div class="pokemon-type-id">
-                        <h2>#${currentPokemon['id']}</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="pokemoncard card-bottom">
-            <div class="cb-header">
-                <div class="cb-about" id="info-about-header" onclick="chooseBoxInOpenCard()"><h3>About</h3></div>
-                <div class="cb-stats cb-inactiv" id="info-stats-header" onclick="chooseBoxInOpenCard()"><h3>Stats</h3></div>
-            </div>
-            <div class="cb-charakter" id="info-about">
-                <div class="cb-charakter-header">
-                    <h4>Height:</h4>
-                    <h4>Weight:</h4>
-                    <h4>Type:</h4>
-                    <h4>Ability:</h4>
-                </div>
-
-                <div class="cb-charakter-infos">
-                    <h4>${currentPokemon['height']}</h4>
-                    <h4>${currentPokemon['weight']}</h4>
-                    <h4>${currentPokemon['types'][0]['type']['name']}, ${currentPokemon['types'][1]['type']['name']}</h4>
-                    <h4>${currentPokemon['abilities'][0]['ability']['name']} / ${currentPokemon['abilities'][1]['ability']['name']}</h4>
-                </div>
-            </div>
-
-
-            <div class="cb-charakter d-none" id="info-stats">
-                <div class="cb-charakter-header">
-                    <h4></h4>
-                    <h4></h4>
-                    <h4></h4>
-                    <h4></h4>
-                </div>
-
-                <div class="cb-charakter-infos">
-                    <h4>${currentPokemon['height']}</h4>
-                    <h4>${currentPokemon['weight']}</h4>
-                    <h4>${currentPokemon['types'][0]['type']['name']}, ${currentPokemon['types'][1]['type']['name']}</h4>
-                    <h4>${currentPokemon['abilities'][0]['ability']['name']} / ${currentPokemon['abilities'][1]['ability']['name']}</h4>
-                </div>
-            </div>
-
-            <div class="cb-buttons">
-                <button>Last Card</button>
-                <button>Next Card</button>
-            </div>
-        </div>
-    </div>
-`;
+        if (!pokemonName.includes(search)) {
+            pokemonCard.classList.add('d-none');
+        }
+        else {
+            if (pokemonCard.classList.contains('d-none')) {
+                pokemonCard.classList.remove('d-none');
+            }
+        }
+    }
 }
---
+++ templates.js
@@ -0,0 +1,96 @@
+/**
+ * Generate HTML
+ */
+function generatePokemonInfoHTML(i) {
+    return /*html*/`
+    <div onclick="openCard(${i})" class="pokemonCard" id="pokemonCard${i}" style="background-color: var(--bg-${currentPokemon['types'][0]['type']['name']})">
+        <div class="pokemon-header">
+            <h2 id="pokemonName${i}">${currentPokemon['name']}</h2>
+        </div>
+        <div class="img-box">
+            <img class="pokemon-img" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}">
+        </div>
+        <div class="pokemon-type-id-box">
+            <div class="pokemon-type-id">
+                <h2>${currentPokemon['types'][0]['type']['name']}</h2>
+            </div>
+            <div class="pokemon-type-id">
+                <h2>#${currentPokemon['id']}</h2>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+
+function generateOpenCardHTML(i) {
+    return /*html*/ `
+    <div class="big-card-bg" id="big-card">
+        <div class="openCard big-card margin-0 width-400" id="pokemonCard${i}" style="background-color: var(--bg-${currentPokemon['types'][0]['type']['name']})">
+            <div class="card-top">
+                <div class="header-icons">
+                    <img src="img/save.svg">
+                    <img onclick="closeCard(${i})" src="img/x-mark.png">
+                </div>
+
+                <div class="pokemon-header">
+                    <h2 class="header-open-card">${currentPokemon['name']}</h2>
+                </div>
+                <div class="img-box width-400">
+                    <img class="pokemon-img" src="${currentPokemon['sprites']['other']['dream_world']['front_default']}">
+                </div>
+                <div class="pokemon-type-id-box">
+                    <div class="pokemon-type-id type-margin">
+                        <h2>${currentPokemon['types'][0]['type']['name']}</h2>
+                    </div>
+                    <div class="pokemon-type-id">
+                        <h2>#${currentPokemon['id']}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pokemoncard card-bottom">
+            <div class="cb-header">
+                <div class="cb-about" id="info-about-header" onclick="chooseBoxInOpenCard()"><h3>About</h3></div>
+                <div class="cb-stats cb-inactiv" id="info-stats-header" onclick="chooseBoxInOpenCard()"><h3>Stats</h3></div>
+            </div>
+            <div class="cb-charakter" id="info-about">
+                <div class="cb-charakter-header">
+                    <h4>Height:</h4>
+                    <h4>Weight:</h4>
+                    <h4>Type:</h4>
+                    <h4>Ability:</h4>
+                </div>
+
+                <div class="cb-charakter-infos">
+                    <h4>${currentPokemon['height']}</h4>
+                    <h4>${currentPokemon['weight']}</h4>
+                    <h4>${currentPokemon['types'][0]['type']['name']}, ${currentPokemon['types'][1]['type']['name']}</h4>
+                    <h4>${currentPokemon['abilities'][0]['ability']['name']} / ${currentPokemon['abilities'][1]['ability']['name']}</h4>
+                </div>
+            </div>
+
+
+            <div class="cb-charakter d-none" id="info-stats">
+                <div class="cb-charakter-header">
+                    <h4></h4>
+                    <h4></h4>
+                    <h4></h4>
+                    <h4></h4>
+                </div>
+
+                <div class="cb-charakter-infos">
+                    <h4>${currentPokemon['height']}</h4>
+                    <h4>${currentPokemon['weight']}</h4>
+                    <h4>${currentPokemon['types'][0]['type']['name']}, ${currentPokemon['types'][1]['type']['name']}</h4>
+                    <h4>${currentPokemon['abilities'][0]['ability']['name']} / ${currentPokemon['abilities'][1]['ability']['name']}</h4>
+                </div>
+            </div>
+
+<!--             <div class="cb-buttons">
+                <button>Last Card</button>
+            </div>
+        </div> -->
+    </div>
+`;
+}
